639ef475670bddd24ee54ede6d566c55cd8dc4a0
Ludovic Woronoff <ludovicworonoff@gmail.com>
2017-02-09 16:34:32 +0100

    toto

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,43 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @ingredient = Ingredient.find_by(name:params[:dose][:ingredient])
+    @dose = Dose.new
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    @dose.description = params[:dose][:description]
+    @dose.save
+    @cocktail.save
+    @ingredient.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  # def show
+  #   @dose = Dose.find(params[:id])
+  # end
+
+  # def index
+  #   @doses = Dose.all
+  # end
+
+  def destroy
+
+  end
+
+  private
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient)
+  end
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cocktails do
+  resources :cocktails, only: [:create, :show, :index, :new] do
     resources :doses
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
