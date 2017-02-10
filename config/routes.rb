7d1e8b49d424e0be50057859f95763a8fce11221
Ludovic Woronoff <ludovicworonoff@gmail.com>
2017-02-10 11:40:24 +0100

    new routes for ingredient

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'cocktail#index'
   resources :cocktails, only: [:create, :show, :index, :new] do
-    resources :doses
+    resources :doses, only: :create
   end
+  resources :doses, only: :destroy
+  resources :ingredients, only: :show
+  root "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
