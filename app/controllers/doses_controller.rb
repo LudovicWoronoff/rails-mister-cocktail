3f1c6f65fc61b756cdf1eaebb6b1b15aa16f2d91
Ludovic Woronoff <ludovicworonoff@gmail.com>
2017-02-10 11:37:54 +0100

    add ingredient show

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,45 +1,32 @@
 class DosesController < ApplicationController
-  before_action :set_cocktail, only: [:new, :create]
-  def new
-    @dose = Dose.new
-  end
+
+
 
   def create
-    @ingredient = Ingredient.find_by(name:params[:dose][:ingredient])
-    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    @dose.ingredient = @ingredient
-    @dose.description = params[:dose][:description]
-    @ingredient.save
-    @cocktail.save
     if @dose.save
     redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render "cocktails/show"
     end
   end
 
-  # def show
-  #   @dose = Dose.find(params[:id])
-  # end
-
-  # def index
-  #   @doses = Dose.all
-  # end
-
   def destroy
-
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(cocktail)
   end
+
 
   private
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:cocktail_id])
-  end
-
   def dose_params
-    params.require(:dose).permit(:description, :ingredient)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
+
 
 
 
